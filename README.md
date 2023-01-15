d07a8462fd7f14dc20b0df98c1f7d308d2741986
Jose Enrique <jbenitez@redgage.com>
2023-01-15 15:38:48 -0500

    Testing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Getting Started with Create React App
+# Reference
 
  [How dockerize](https://dev.to/thexdev/dockerize-production-reactjs-3ai9).
+ [React Router](https://blog.logrocket.com/complete-guide-authentication-with-react-router-v6).
+ 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,17 @@
 
 export default function Home() {
-  return (<h1>Home</h1>)
+  return (
+    <>
+      <h1>Home</h1>
+      <button className="bg-violet-800 hover:bg-violet-500 active:bg-violet-900 focus:outline-none focus:ring focus:ring-violet-300">Demo</button>
+      <blockquote className="text-2xl font-semibold italic text-center text-slate-900">
+        When you look
+        <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+          <span className="relative text-white">annoyed</span>
+        </span>
+        all the time, people think that you're busy.
+      </blockquote>
+    </>
+
+  )
 }
