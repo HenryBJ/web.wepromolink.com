dabfb0b0f46dde3a5ca11dcfc9afae84b4fee26a
Jose Enrique <jbenitez@redgage.com>
2023-01-15 17:03:37 -0500

    Theme color set

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,11 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <button className="bg-violet-800 hover:bg-violet-500 active:bg-violet-900 focus:outline-none focus:ring focus:ring-violet-300">Demo</button>
-      <blockquote className="text-2xl font-semibold italic text-center text-slate-900">
-        When you look
-        <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-          <span className="relative text-white">annoyed</span>
-        </span>
-        all the time, people think that you're busy.
-      </blockquote>
+      <div className="columns-1 md:columns-2 md:hover:columns-3 lg:columns-3 gap-8">
+        <div className="bg-red-500">Demo</div>
+        <div className="bg-blue-500">Demo</div>
+        <div className="bg-green-300">Demo</div>
+      </div>
     </>
 
   )
