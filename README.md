cf68cb88eeb86ccf121898114a88d5b88815a0a5
Jose Enrique <jbenitez@redgage.com>
2023-01-23 17:11:43 -0500

    Add Terms

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
  [How dockerize](https://dev.to/thexdev/dockerize-production-reactjs-3ai9).
  [React Router](https://blog.logrocket.com/complete-guide-authentication-with-react-router-v6).
  [Hamburger Menu](https://hamburger-react.netlify.app/)
+ [Icoins](https://heroicons.com/)
+ [UI Inspiration](https://tailwind-elements.com/)
+ [Another UI Inspiration](https://headlessui.com/)
+ [Tailwind Cheat sheet](https://nerdcave.com/tailwind-cheat-sheet)
+ [Figma design](https://www.figma.com/file/o2XrADxEmLeREP6PAyB1Tq/WePromoLink?node-id=0%3A1&t=VZv3mRTucSOopFtQ-0)
---
+++ src/pages/Terms/terms.tsx
@@ -0,0 +1,47 @@
+
+export default function TermsAndConditions(){ return  {
+    termsAndConditions: [
+      {
+        topic: "Introduction",
+        text: "These terms and conditions (\"Terms\") apply to the use of the website wepromolink.com (\"Website\") and the services provided through the Website. By accessing or using the Website, you agree to be bound by these Terms. If you do not agree to these Terms, you may not use the Website."
+      },
+      {
+        topic: "Use of Website",
+        text: "You may use the Website for lawful purposes only. You may not use the Website for any unauthorized or illegal activity, or to promote or encourage such activity."
+      },
+      {
+        topic: "Account Registration",
+        text: "In order to access certain features of the Website, you may be required to register for an account. You are responsible for maintaining the confidentiality of your account information, including your username and password."
+      },
+      {
+        topic: "Content",
+        text: "The Website may display or provide access to various types of content, including but not limited to text, images, videos, and user-generated content. You are responsible for any content you submit or post to the Website."
+      },
+      {
+        topic: "Intellectual Property",
+        text: "The Website and its content are protected by copyright, trademark, and other intellectual property laws. You may not use the Website or its content in any way that infringes the rights of the Website or any third party."
+      },
+      {
+        topic: "Disclaimer",
+        text: "The Website and its content are provided \"as is\" without warranty of any kind, either express or implied. The Website and its owners make no representations or warranties of any kind, either express or implied, as to the accuracy, completeness, or reliability of the content of the Website."
+      },
+      {
+        topic: "Limitation of Liability",
+        text: "The Website and its owners will not be liable for any damages of any kind arising from the use of the Website or its content, including but not limited to direct, indirect, incidental, punitive, and consequential damages."
+      },
+      {
+        topic: "Changes to Terms",
+        text: "The Website may change these Terms at any time, and any changes will be effective immediately upon posting to the Website. Your continued use of the Website after any changes to these Terms will indicate your acceptance of the new Terms."
+      },
+      {
+        topic: "Governing Law",
+        text: "These Terms shall be governed by and construed in accordance with the laws of the jurisdiction in which the Website is located, without giving effect to any principles of conflicts of law."
+      },
+      {
+        topic: "Contact Us",
+        text: "If you have any questions about these Terms, please contact us through the contact information provided on the Website."
+      }
+    ]
+  }
+};
+  
