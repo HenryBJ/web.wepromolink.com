1272e2a62d95fe5600956f6ee051acc6eedc1538
Jose Enrique <jbenitez@redgage.com>
2023-01-26 17:43:53 -0500

    Fix contact page height

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -7,15 +7,15 @@ export default function Contact() {
   return (
     <div className="container max-w-3xl mx-auto flex gap-8 items-center flex-col justify-start  text-orange-100 md:text-orange-800  z-50  pt-12 md:pt-0 md:mt-5 md:h-[calc(100vh-110px)]">
       <h1 className="font-bold text-3xl text-center text-orange-100 md:text-orange-800 mb-4">Contact us</h1>
-      <div className="rounded-full w-32 h-32 overflow-clip ring-2 ring-white md:ring-orange-500 relative">
+      <div className="rounded-full flex-shrink-0 w-28 h-28 overflow-clip ring-2 ring-white md:ring-orange-500 relative">
         <img src="/images/founder.png" alt="Founder"  />  
       </div>
      
-      <div className="mx-4">
-        <p className=" text-orange-100 md:text-orange-800 text-justify text-lg overflow-y-auto md:h-96 h-60 ">
+      <div className="mx-4 flex-shrink h-1/4">
+        <p className="px-2 text-orange-100 md:text-orange-800 text-justify text-lg overflow-y-auto md:h-full min-h-0 h-60 ">
         {Bio}
         </p>
-        <div className="mt-4  text-center">
+        <div className="mt-10  text-center">
           <a href="#">
             <FontAwesomeIcon icon={faFacebook} className=" text-orange-100 md:text-orange-500 mr-4 text-3xl hover:text-orange-800" />
           </a>
