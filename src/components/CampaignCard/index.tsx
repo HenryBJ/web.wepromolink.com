f42f20527eea6454dd28faa62638b9b8d742798c
Jose Enrique <jbenitez@redgage.com>
2023-01-29 18:38:02 -0500

    WIP: dashboard v1.0 1

--- src/components/CampaignCard/index.tsx
+++ src/components/CampaignCard/index.tsx
@@ -1,30 +1,30 @@
-import { faMoneyBill, faMoneyBill1, faMoneyBill1Wave, faMoneyBillAlt, faMoneyBillTransfer, faMoneyBillWheat, faWallet } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ICampaingCard } from "../../interfaces/ICampaingCard";
 
 
 export default function CampaignCard(data: ICampaingCard) {
     return (
         <>
-            <div className="max-w-sm rounded-md overflow-hidden shadow-lg bg-white ">
-                <img className="w-full" src={data.imageUrl} alt="Campaign image" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{data.title}</div>
-                    <p className="text-gray-700 text-base">
-                        {data.description}
-                    </p>
+            <div className="max-w-sm w-full lg:max-w-full lg:flex">
+                <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${data.imageUrl})`}} title={data.title}>
                 </div>
-                <div className="px-6 pt-4 pb-2 flex gap-2 text-gray-600">
-                    <span className=" items-center flex gap-1">
-                        <FontAwesomeIcon icon={faWallet} />
-                        ${data.budget}
-                    </span>
-
-                    <span className=" items-center flex gap-1">
-                        <FontAwesomeIcon icon={faMoneyBill1Wave} />
-                        ${data.epm}/1k clicks
-                    </span>
-
+                <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8">
+                        <p className="text-sm text-gray-600 flex items-center">
+                            <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                            </svg>
+                            Members only
+                        </p>
+                        <div className="text-gray-900 font-bold text-xl mb-2">{data.title}</div>
+                        <p className="text-gray-700 text-base">{data.description}</p>
+                    </div>
+                    {/* <div className="flex items-center">
+                        <img className="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink"/>
+                            <div className="text-sm">
+                                <p className="text-gray-900 leading-none">Jonathan Reinink</p>
+                                <p className="text-gray-600">Aug 18</p>
+                            </div>
+                    </div> */}
                 </div>
             </div>
         </>
