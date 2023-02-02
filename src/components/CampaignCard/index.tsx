17ca9f9d9e93b0c5aaa6c411679c6f60d607a62f
Jose Enrique <jbenitez@redgage.com>
2023-02-02 17:15:08 -0500

    WIP: dashboard fixing error

--- src/components/CampaignCard/index.tsx
+++ src/components/CampaignCard/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ICampaingCard } from "../../interfaces/ICampaingCard";
+import ImageCache from "../ImageCache";
 import ShareDialog from "../ShareDialog";
 
 
@@ -20,7 +21,7 @@ export default function CampaignCard(data: ICampaingCard) {
                 <div className="lg:flex justify-between w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4">
                     <div className="flex flex-col justify-between leading-normal">
                         <div className="mb-8">
-                            <p className="text-sm text-gray-600 flex items-center">
+                            <div className="text-sm text-gray-600 flex items-center">
                                 {/* <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                                 </svg>
@@ -29,7 +30,7 @@ export default function CampaignCard(data: ICampaingCard) {
                                 <div className="text-sm">
                                     <p className="text-gray-600 leading-none">{data.autorName}</p>
                                 </div>
-                            </p>
+                            </div>
                             <div className="text-gray-900 font-bold text-xl mb-2">{data.title}</div>
                             <p className="text-gray-700 text-base">{data.description}</p>
                         </div>
