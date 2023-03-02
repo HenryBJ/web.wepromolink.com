5bcd3e4227865795f23995a6e174df8c243d3860
Jose Enrique <jbenitez@redgage.com>
2023-03-02 14:48:51 -0500

    Price page upgraded

---
+++ src/components/SubcriptionCard/index.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { faBtc, faCcMastercard, faCcVisa, faPaypal, faStripe } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+interface IProps {
+    title: string,
+    monthly: number,
+    annually: number,
+    discount: number,
+    depositFee?: number,
+    payoutFee?: number,
+    payoutMinimun?: number,
+    ads?: boolean,
+    visa?: boolean,
+    mastercard?: boolean,
+    stripe?: boolean,
+    bitcoin?: boolean,
+    paypal?: boolean,
+    wireBank?: boolean,
+    tag?: string,
+    onGetStarted:(monthly:boolean)=>void
+
+}
+
+const checkIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-orange-500">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+</svg>
+
+const NoIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
+
+
+
+
+
+export default function Index({ title, monthly, annually, discount, depositFee, payoutFee, ads, visa, mastercard, stripe, bitcoin, paypal, wireBank, tag, payoutMinimun = 0, onGetStarted }: IProps) {
+
+    const [proMonthly, setProMonthly] = useState(true);
+
+    return (
+        <div className="relative overflow-clip shadow-2xl h-96 rounded-md ">
+            <div className="absolute bottom-0 left-0 w-full px-4">
+                <button onClick={()=>onGetStarted(proMonthly)} className="w-full hover:bg-orange-700 cursor-pointer  bg-orange-500 text-white font-sans font-bold text-center text-base px-4 py-1 mx-auto my-4 rounded-full shadow-xl hover:shadow-none active:bg-white active:text-orange-500 active:ring-2 active:ring-orange-500">
+                    Get Started
+                </button>
+            </div>
+
+            <div className="bg-gray-100 md:bg-white/60 h-96 w-72 rounded-md flex flex-col p-3">
+                <div className="w-full text-center text-orange-800 text-2xl">{title}</div>
+
+                {proMonthly ?
+                    <div className="w-full text-center text-orange-800 text-xl mt-2">{`$${monthly}/mo`}</div>
+                    :
+                    <div className="relative mt-2">
+                        <div className="w-full text-center text-orange-800 text-xl">{`$${annually}/year`}</div>
+                        {discount !== 0 && <span className="absolute -bottom-3 right-12 text-white rounded text-xs bg-orange-500 font-bold px-1">{`${discount}% off`}</span>}
+                    </div>
+                }
+
+                {(monthly !== 0 && annually !== 0) ? <div className="w-full flex justify-around mt-3">
+                    <span className={proMonthly ? 'text-orange-500 font-bold' : ' text-gray-400'}>Monthly</span>
+                    <input
+                        className="mt-[0.3rem] mr-2 h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-[rgba(0,0,0,0.25)] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
+                        type="checkbox"
+                        role="switch" onChange={() => setProMonthly(!proMonthly)} />
+                    <span className={!proMonthly ? 'text-orange-500 font-bold' : ' text-gray-400'}>Annually</span>
+                </div> : <div className="mt-9"></div>}
+
+                <table className="w-full mt-1 table">
+                    <tr>
+                        <td className="w-9/10 text-orange-800">Deposit fee</td>
+                        <td className="w-1/10 text-right text-orange-800">{depositFee ? `${depositFee}%` : '0%'}</td>
+                    </tr>
+                    <tr>
+                        <td className="w-9/10 text-orange-800">Payout fee</td>
+                        <td className="w-1/10 text-right text-orange-800">{payoutFee ? `${payoutFee}%` : '0%'}</td>
+                    </tr>
+                    <tr>
+                        <td className="w-9/10 text-orange-800">Payment method</td>
+                        <td className="w-1/10 flex justify-end text-orange-800">
+                            <div className="flex gap-1">
+                                {visa && <FontAwesomeIcon icon={faCcVisa} className="text-xl text-blue-600" />}
+                                {mastercard && <FontAwesomeIcon icon={faCcMastercard} className="text-xl text-red-600" />}
+                                {stripe && <FontAwesomeIcon icon={faStripe} className="text-xl text-blue-600" />}
+                                {bitcoin && <FontAwesomeIcon icon={faBtc} className="text-xl text-yellow-500" />}
+                                {paypal && <FontAwesomeIcon icon={faPaypal} className="text-xl text-blue-600" />}
+                                {wireBank && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-orange-500">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                                </svg>
+                                }
+                            </div>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="w-9/10 text-orange-800">Payout minimun</td>
+                        <td className="w-1/10 text-right text-orange-800">{`$${payoutMinimun}`}</td>
+                    </tr>
+                    <tr>
+                        <td className="w-9/10 text-orange-800">Contain Ads</td>
+                        <td className="w-1/10 text-right text-orange-800  flex justify-end">{ads ? checkIcon : NoIcon}</td>
+                    </tr>
+                </table>
+
+            </div>
+            {tag && <span className="absolute top-5 -right-8 bg-orange-500 py-0 px-10 text-white text-md font-semibold shadow-md rotate-45">{tag}</span>}
+        </div>
+    )
+}
--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -1,126 +1,55 @@
 import { useState } from "react";
+import SubcriptionCard from "../../components/SubcriptionCard";
 
 export default function Pricing() {
 
   const [proMonthly, setProMonthly] = useState(true);
   const [busiMonthly, setBusiMonthly] = useState(true);
 
+  const onGetStarted = (monthly:boolean)=>{
+    alert(monthly)
+  }
+
   return (
 
     <div className="container max-w-4xl mx-auto z-50 pt-12 md:pt-0 md:mt-5">
       <h1 className="font-bold text-3xl text-center text-orange-100 md:text-orange-800 mb-4">Pricing</h1>
 
-      <div className=" h-full w-full flex flex-wrap gap-3">
-        <div className="bg-white/60 h-96 w-72 rounded-md shadow-2xl flex flex-col p-3">
-          <div className="w-full text-center text-orange-800 text-2xl">Community</div>
-          <div className="w-full text-center text-orange-800 text-xl mt-2">$0/mo</div>
-          <table className="w-full mt-10 table">
-            <tr>
-              <td className="w-9/10 text-orange-800">Deposit fee</td>
-              <td className="w-1/10 text-right text-orange-800">5%</td>
-            </tr>
-            <tr>
-              <td className="w-9/10 text-orange-800">Payout fee</td>
-              <td className="w-1/10 text-right text-orange-800">5%</td>
-            </tr>
-            <tr>
-              <td className="w-9/10 text-orange-800">Payment method</td>
-              <td className="w-1/10 text-right text-orange-800">Bitcoin</td>
-            </tr>
-            <tr>
-              <td className="w-9/10 text-orange-800">Payout minimun</td>
-              <td className="w-1/10 text-right text-orange-800">$100</td>
-            </tr>
-          </table>
+      <div className=" h-[calc(100vh-120px)] md:h-[calc(100vh-180px)] w-full flex flex-wrap gap-3 md:gap-11 justify-center overflow-y-auto ">
 
-        </div>
+        <SubcriptionCard
+          annually={0}
+          monthly={0}
+          discount={0}
+          title="Community"
+          ads={true}
+          depositFee={5}
+          payoutFee={5}
+          bitcoin={true}
+          paypal={false}
+          visa={false}
+          payoutMinimun={100}
+          mastercard={false}
+          onGetStarted={onGetStarted}
+          wireBank={false} />
 
-        <div className="relative overflow-clip">
-          <div className="bg-white/60 h-96 w-72 rounded-md shadow-2xl flex flex-col p-3">
-            <div className="w-full text-center text-orange-800 text-2xl">Professional</div>
-
-            {proMonthly ?
-              <div className="w-full text-center text-orange-800 text-xl mt-2">$4.99/mo</div>
-              :
-              <div className="relative mt-2">
-                <div className="w-full text-center text-orange-800 text-xl">$54.89/year</div>
-                <span className="absolute -bottom-2 right-12 text-white rounded text-[9px] bg-orange-500 font-bold px-1">8.34% off</span>
-              </div>
-            }
-
-            <div className="w-full flex justify-around mt-3">
-              <span className={proMonthly ? 'text-orange-500 font-bold' : ' text-gray-400'}>Monthly</span>
-              <input
-                className="mt-[0.3rem] mr-2 h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-[rgba(0,0,0,0.25)] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
-                type="checkbox"
-                role="switch" onChange={() => setProMonthly(!proMonthly)} />
-              <span className={!proMonthly ? 'text-orange-500 font-bold' : ' text-gray-400'}>Annually</span>
-            </div>
-
-            <table className="w-full mt-1 table">
-              <tr>
-                <td className="w-9/10 text-orange-800">Deposit fee</td>
-                <td className="w-1/10 text-right text-orange-800">0%</td>
-              </tr>
-              <tr>
-                <td className="w-9/10 text-orange-800">Payout fee</td>
-                <td className="w-1/10 text-right text-orange-800">0%</td>
-              </tr>
-              <tr>
-                <td className="w-9/10 text-orange-800">Payment method</td>
-                <td className="w-1/10 text-right text-orange-800">Visa, Mastercard, Stripe</td>
-              </tr>
-              <tr>
-                <td className="w-9/10 text-orange-800">Payout minimun</td>
-                <td className="w-1/10 text-right text-orange-800">$50</td>
-              </tr>
-            </table>
-
-          </div>
-          <span className="absolute top-5 -right-8 bg-orange-500 py-0 px-10 text-white text-md font-semibold rotate-45">Popular</span>
-        </div>
-
-
-        <div className="bg-white/60 h-96 w-72 rounded-md shadow-2xl flex flex-col p-3">
-          <div className="w-full text-center text-orange-800 text-2xl">Bussiness</div>
-          {busiMonthly ?
-            <div className="w-full text-center text-orange-800 text-xl mt-2">$14.99/mo</div>
-            :
-            <div className="relative mt-2">
-              <div className="w-full text-center text-orange-800 text-xl">$149.90/year</div>
-              <span className="absolute -bottom-2 right-12 text-white rounded text-[9px] bg-orange-500 font-bold px-1">16.67% off</span>
-            </div>
-          }
-
-          <div className="w-full flex justify-around mt-3">
-            <span className={busiMonthly ? 'text-orange-500 font-bold' : ' text-gray-400'}>Monthly</span>
-            <input
-              className="mt-[0.3rem] mr-2 h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-[rgba(0,0,0,0.25)] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
-              type="checkbox"
-              role="switch" onChange={() => setBusiMonthly(!busiMonthly)} />
-            <span className={!busiMonthly ? 'text-orange-500 font-bold' : ' text-gray-400'}>Annually</span>
-          </div>
-
-          <table className="w-full mt-1 table">
-            <tr>
-              <td className="w-9/10 text-orange-800">Deposit fee</td>
-              <td className="w-1/10 text-right text-orange-800">0%</td>
-            </tr>
-            <tr>
-              <td className="w-9/10 text-orange-800">Payout fee</td>
-              <td className="w-1/10 text-right text-orange-800">0%</td>
-            </tr>
-            <tr>
-              <td className="w-9/10 text-orange-800">Payment method</td>
-              <td className="w-1/10 text-right text-orange-800">Visa, Mastercard, Stripe</td>
-            </tr>
-            <tr>
-              <td className="w-9/10 text-orange-800">Payout minimun</td>
-              <td className="w-1/10 text-right text-orange-800">$50</td>
-            </tr>
-          </table>
-        </div>
-
+        <SubcriptionCard
+          annually={54.89}
+          monthly={4.99}
+          discount={8.34}
+          title="Professional"
+          tag="Popular"
+          ads={false}
+          depositFee={0}
+          payoutFee={0}
+          stripe={true}
+          bitcoin={false}
+          paypal={false}
+          visa={true}
+          payoutMinimun={50}
+          mastercard={true}
+          onGetStarted={onGetStarted}
+          wireBank={false} />
 
       </div>
 
