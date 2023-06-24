2d27fcfd83164901ec2028e1d2ec5aea33142e67
asosa <asosabenitez1991@gmail.com>
2023-06-24 19:04:36 -0300

    feat: add link to social icons

--- src/components/SocialNetworks/index.tsx
+++ src/components/SocialNetworks/index.tsx
@@ -4,27 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Index(){
 
     const style = 'text-white mr-2 ml-2 text-2xl hover:text-orange-800';
-    
+
+    const socials = [
+        //{icon: faFacebook, href: "https://www.facebook.com/yourusername"},
+        {icon: faTwitter, href: "https://twitter.com/WePromoLink"},
+        {icon: faTelegram, href: "https://t.me/wepromolink_support"},
+        {icon: faWhatsapp, href: "https://chat.whatsapp.com/Itdy4Fgx9ld3UYClzUCPU6"},
+        //{icon: faInstagram, href: "https://www.instagram.com/yourusername"},
+        //{icon: faPinterest, href: "https://www.pinterest.com/yourusername"},
+    ];
+
     return (
         <div className="mt-10  text-center">
-          <a href="#">
-            <FontAwesomeIcon icon={faFacebook} className={style} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faTwitter} className={style} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faTelegram} className={style} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faWhatsapp} className={style} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faInstagram} className={style} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faPinterest} className={style} />
-          </a>
+            {socials.map((social, index) => (
+                <a key={index} href={social.href} target="_blank">
+                    <FontAwesomeIcon icon={social.icon} className={style}/>
+                </a>
+            ))}
         </div>
     )
 }
