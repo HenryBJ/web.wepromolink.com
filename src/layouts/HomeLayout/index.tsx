68efb06888f492fe20434319cb6d1fb265713d90
Jose E. Benitez <jose.devops@gmail.com>
2024-03-30 23:44:43 -0400

    fix bug iphone

--- src/layouts/HomeLayout/index.tsx
+++ src/layouts/HomeLayout/index.tsx
@@ -63,7 +63,7 @@ export default function HomeLayout() {
                         <div className='absolute bottom-3 flex items-center justify-center w-full'>
                             <SocialNetworks />
                         </div>
-                        <span className='absolute bottom-0 text-xs text-white/50 w-full text-right mr-1'>v1.0.130</span>
+                        <span className='absolute bottom-0 text-xs text-white/50 w-full text-right mr-1'>v1.0.131</span>
                     </div>
                 </div>
                 <div className='grow md:hidden h-[calc(100vh-74px)]'>
